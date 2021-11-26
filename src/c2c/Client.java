120c26e59862e8256df9cf697824a2a5b1b9bbf8
Mullai Rajan <86584609+MullaiArgent@users.noreply.github.com>
2021-11-26 15:25:14 +0530

    csc update

---
+++ src/c2c/Client.java
@@ -0,0 +1,82 @@
+package c2c;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private Socket socket;
+    private BufferedWriter bufferedWriter;
+    private BufferedReader bufferedReader;
+    private String userName;
+    public Client (Socket socket, String userName) throws IOException {
+        try {
+            this.socket = socket;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.userName = userName;
+        }catch (IOException e){
+            closeEverything(socket, bufferedWriter, bufferedReader);
+        }
+    }
+
+    private void closeEverything(Socket socket, BufferedWriter bufferedWriter, BufferedReader bufferedReader) {
+        try{
+            if(socket != null){
+                socket.close();
+            }if (bufferedReader != null){
+                bufferedReader.close();
+            }if (bufferedWriter != null){
+                bufferedWriter.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage(){
+        try{
+            bufferedWriter.write(userName);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+            Scanner scanner = new Scanner(System.in);
+            while(socket.isConnected()){
+                String messageToSend = scanner.nextLine();
+                bufferedWriter.write(messageToSend);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+        }catch (IOException e){
+            closeEverything(socket, bufferedWriter, bufferedReader);
+        }
+    }
+
+    public void listenForMessage(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String msgFromGroupChat;
+                while(socket.isConnected()){
+                    try {
+                        msgFromGroupChat = bufferedReader.readLine();
+                        System.out.println(msgFromGroupChat);
+
+                    } catch (IOException e) {
+                        closeEverything(socket, bufferedWriter, bufferedReader);
+                    }
+
+                }
+            }
+        }).start();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your user name");
+        String username = scanner.nextLine();
+        Socket socket = new Socket("localhost", 1234);
+        Client client = new Client(socket, username);
+        client.listenForMessage();
+        client.sendMessage();
+    }
+}
---
+++ src/c2c/ClientHandler.java
@@ -0,0 +1,76 @@
+package c2c;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable {
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    BufferedReader bufferedReader;
+    BufferedWriter bufferedWriter;
+    String clientUserName;
+    public ClientHandler(Socket socket) throws IOException {
+        try {
+            this.socket = socket;
+            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            clientUserName = bufferedReader.readLine();
+            clientHandlers.add(this);
+            broadcastMessage("Server :" + clientUserName + "has entered the chat");
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void broadcastMessage(String messageFromClient) {
+        for (ClientHandler clientHandler : clientHandlers){
+            try{
+                if(!clientHandler.clientUserName.equals(clientUserName)){
+
+                    clientHandler.bufferedWriter.write(clientUserName+": "+messageFromClient);
+                    clientHandler.bufferedWriter.newLine();
+                    clientHandler.bufferedWriter.flush();
+                }
+            }catch (IOException e){
+                closeEverything(socket, bufferedWriter, bufferedReader);
+            }
+        }
+    }
+
+    private void closeEverything(Socket socket, BufferedWriter bufferedWriter, BufferedReader bufferedReader) {
+        removeClientHandler();
+        try{
+            if(bufferedReader != null){
+                bufferedReader.close();
+            }if (bufferedWriter != null){
+                bufferedWriter.close();
+            }if (socket != null){
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void removeClientHandler() {
+        clientHandlers.remove(this);
+        broadcastMessage("Server : " + clientUserName + " has left the Chat");
+    }
+
+
+    @Override
+    public void run() {
+        String messageFromClient;
+        while(socket.isConnected()){
+            try{
+                messageFromClient = bufferedReader.readLine();
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
